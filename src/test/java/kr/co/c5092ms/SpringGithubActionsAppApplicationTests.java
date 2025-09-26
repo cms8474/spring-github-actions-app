f51607244da4a0a4a86f5876f8d40b7e4f38965f
c5092ms <c5092ms@gmail.com>
2025-09-26 10:10:34 +0900

    commit

---
+++ src/test/java/kr/co/c5092ms/SpringGithubActionsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.c5092ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubActionsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
